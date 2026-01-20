d07aa7493eab0272401f72afd6c6bce02963b2c3
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2026-01-20 23:40:45 +0100

    feat: start working on native api

--- collektive-backend/lib/src/commonMain/kotlin/it/unibo/collektive/unity/shared/Vector3.kt
+++ collektive-backend/lib/src/commonMain/kotlin/it/unibo/collektive/unity/shared/Vector3.kt
@@ -3,11 +3,17 @@ package it.unibo.collektive.unity.shared
 import kotlin.math.pow
 import kotlin.math.sqrt
 
+/**
+ * Computes the geometric distance between this vector and the passed one.
+ */
 fun Vector3.distanceTo(other: Vector3): Float =
     sqrt(
         (this.x - other.x).pow(2) + (this.y - other.y).pow(2) + (this.z - other.z).pow(2)
     )
 
+/**
+ * Computes the geometric distance between the passed vectors.
+ */
 object Vector3Helper {
     fun distance(a: Vector3, b: Vector3) =
         a.distanceTo(b)
---
+++ collektive-backend/lib/src/nativeMain/kotlin/EngineNativeApi.kt
@@ -0,0 +1,68 @@
+package it.unibo.collektive.unity.core
+
+import it.unibo.collektive.unity.data.GlobalData
+import it.unibo.collektive.unity.schema.NodeState
+import it.unibo.collektive.unity.schema.SensorData
+import kotlinx.cinterop.ByteVar
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.CPointerVar
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.IntVar
+import kotlinx.cinterop.allocArray
+import kotlinx.cinterop.get
+import kotlinx.cinterop.nativeHeap
+import kotlinx.cinterop.plus
+import kotlinx.cinterop.pointed
+import kotlinx.cinterop.readBytes
+import kotlinx.cinterop.refTo
+import kotlinx.cinterop.value
+import platform.posix.memcpy
+import kotlin.experimental.ExperimentalNativeApi
+
+var engine: Engine? = null
+
+@OptIn(ExperimentalNativeApi::class, ExperimentalForeignApi::class)
+@CName("initialize")
+fun initialize(dataPointer: CPointer<ByteVar>?, dataSize: Int)
+{
+    require(dataPointer != null) { "Invalid null pointer. Global data pointer should point to valid heap structure" }
+    engine = EngineImpl(
+        NetworkManagerImpl(),
+        GlobalData.ADAPTER.decode(dataPointer.readBytes(dataSize))
+    )
+}
+
+@OptIn(ExperimentalNativeApi::class, ExperimentalForeignApi::class)
+@CName("step")
+fun step(
+    count: Int,
+    pointers: CPointer<CPointerVar<ByteVar>>?,
+    sizes: CPointer<IntVar>?
+): CPointer<CPointerVar<ByteVar>> {
+    requireEngine()
+    require(pointers != null || sizes != null || count > 0)
+    { "Invalid data passed to step function. Either pointers or sizes are null or count is less than zero" }
+    val sensingData = List(count) { i ->
+        val dataPtr = pointers?.get(i)
+        val dataSize = sizes?.get(i)
+        require(dataSize != null) { "A value inside the sizes list was null (index: $i)" }
+        require(dataPtr != null) { "A value inside the data pointers list was null (index: $i)" }
+        SensorData.ADAPTER.decode(dataPtr.readBytes(dataSize))
+    }
+    val nodeStates = engine?.step(sensingData)!!
+    val results: List<ByteArray> = nodeStates.map { NodeState.ADAPTER.encode(it) }
+    return prepareReturnData(results)
+}
+
+private fun requireEngine() = require(engine != null) { "The engine is null" }
+
+@OptIn(ExperimentalForeignApi::class)
+private fun prepareReturnData(results: List<ByteArray>): CPointer<CPointerVar<ByteVar>> {
+    val resultPointers = nativeHeap.allocArray<CPointerVar<ByteVar>>(results.size)
+    results.forEachIndexed { i, bytes ->
+        val nativeBytes = nativeHeap.allocArray<ByteVar>(bytes.size)
+        memcpy(nativeBytes, bytes.refTo(0), bytes.size.toULong())
+        resultPointers.plus(i)!!.pointed.value = nativeBytes
+    }
+    return resultPointers
+}
