706946a75696ea65c248096a774ceeee1106a701
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2026-03-21 18:34:28 +0100

    feat: create proximity neighboring component

---
+++ Collektive.Unity/Runtime/Example/ProximityNeighboring.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using Collektive.Unity.CollectiveNode;
+using UnityEngine;
+
+namespace Collektive.Unity.Example
+{
+    public class ProximityNeighboring : NeighboringComponent
+    {
+        private void Start()
+        {
+            Debug.Assert(
+                GetComponents<Collider>().Any(c => c.isTrigger),
+                "ProximityNeighboring: proximityCollider's GameObject must have at least one trigger collider.",
+                this
+            );
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<Node>(out var node))
+                SubscribeTo(node);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent<Node>(out var node))
+                UnsubscribeFrom(node);
+        }
+    }
+}
